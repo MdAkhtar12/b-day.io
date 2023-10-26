d709e912ff1947f3cde8b2b3211772834b383753
Md Shahnewaz Akhtar <shahnawazakhtar1998@gmail.com>
2023-10-27 01:04:48 +0530

    bday

---
+++ script.js
@@ -0,0 +1,5 @@
+let navigationEl = document.querySelector(".navigation");
+let openEl = document.querySelector(".open")
+navigationEl.onclick = function(){
+    navigationEl.classList.toggle("active")
+}
